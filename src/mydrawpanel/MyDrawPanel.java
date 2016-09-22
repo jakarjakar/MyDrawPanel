736ce31ae9d100ec9a9b7ec71a0e32967aa5ce7e
Artsiom_Berazavik <Artsiom_Berazavik@epbyminw3766.minsk.epam.com>
2016-09-22 18:39:01 +0300

    Ho start this?

---
+++ src/mydrawpanel/MyDrawPanel.java
@@ -0,0 +1,18 @@
+//@Date Sep 21, 2016
+//@author Artsiom_Berazavik
+import java.awt.*;
+import javax.swing.*;
+
+
+public class MyDrawPanel extends JPanel {
+    @Override
+    public void paintComponent(Graphics g) {
+        g.setColor(Color.ORANGE);
+        g.fillRect(20, 50, 100, 100);
+    }
+    
+    public static void main(String[] args) {
+        MyDrawPanel d = new MyDrawPanel();
+        d.paintComponent(g);
+    }
+}
