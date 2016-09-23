a45d59000083ea1d3d90de66976da23e75af7028
Artsiom_Berazavik <Artsiom_Berazavik@epbyminw3766.minsk.epam.com>
2016-09-23 11:59:36 +0300

    Use NetBeans suggestions

--- src/mydrawpanel/MyDrawPanel.java
+++ src/mydrawpanel/MyDrawPanel.java
@@ -5,18 +5,23 @@ import java.awt.*;
 import javax.swing.*;
 
 
-public class MyDrawPanel extends JPanel {
-    
-    //как тут использовать класс MyFrame из пэкаджа mydrawer и переопределить часть кода?
+public class MyDrawPanel extends JPanel  {
     
     @Override
     public void paintComponent(Graphics g) {
         g.setColor(Color.ORANGE);
         g.fillRect(20, 50, 100, 100);
     }
+
+    private static class MyFrameImpl extends MyFrame {
+
+        public MyFrameImpl() {
+        }
+    }    
     
     public static void main(String[] args) {
-        MyFrame f = new MyFrame();
+        MyFrame f = new MyFrameImpl();
         f.CreateFrame();
+     
     }
 }
